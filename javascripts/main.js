243db1e2598b53ed54933b82a98d6aafbd0136a3
nhatlich808 <nhatlich808@gmail.com>
2016-10-08 21:53:49 +0700

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,1 @@
-var App = React.createClass({
-  render() {
-    return(
-      <div>
-      Hello world ! <input type="text" placeholder="Your name here..." />
-      <br/>It's {this.props.date.toTimeString()}
-      </div>
-    );
-  }
-});
-function tick() {
-  ReactDOM.render(<App date={ new Date()} />, document.getElementById('app'));
-}
-setInterval(tick, 500);
+console.log('Hi !');
